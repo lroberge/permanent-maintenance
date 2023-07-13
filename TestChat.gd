35846b04eadd60958a958b6460988941f16c1980
lroberge <31317307+lroberge@users.noreply.github.com>
2023-07-13 15:09:56 -0400

    Test chat-style dialogue formatting
    No character color handling implemented yet - it's still all in BBcode tags

---
+++ TestChat.gd
@@ -0,0 +1,30 @@
+extends Node
+
+var dialogue
+var msg_template
+
+var dialogue_line: DialogueLine:
+	set(next_dialogue_line):
+		dialogue_line = next_dialogue_line
+		var chatline = ""
+		if not dialogue_line.character.is_empty():
+			chatline += dialogue_line.character + ": "
+		chatline += dialogue_line.text + "\n"
+		$ChatText.append_text(chatline)
+		if dialogue_line.time != null:
+			var time = dialogue_line.text.length() * 0.02 if dialogue_line.time == "auto" else dialogue_line.time.to_float()
+			await get_tree().create_timer(time).timeout
+			next(dialogue_line.next_id)
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	dialogue = load("res://test_dialogue.dialogue") as DialogueResource
+	dialogue_line = await dialogue.get_next_dialogue_line("chatting_test")
+
+func next(next_id: String) -> void:
+	self.dialogue_line = await dialogue.get_next_dialogue_line(next_id)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
